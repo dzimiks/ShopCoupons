e66abb1df86736574a47b790cf66ed6153bf0c30
dzimiks <vana997@gmail.com>
2019-06-05 16:34:51 +0200

    Add ES6 syntax

--- web/js/app.js
+++ web/js/app.js
@@ -1,87 +1,88 @@
-let restTable = document.getElementById('rest-table').tBodies[0];
-
 $(document).ready(function () {
-    $.get('rest/shops', function (data) {
+    const restTable = document.getElementById('rest-table').tBodies[0];
+    const form = document.getElementById('add-coupon-form');
+
+    const result = {
+        addCoupon: () => {
+            $.post('rest/coupons', $('#add-coupon-form').serialize(), function (data) {
+                this.addRow(data);
+                $('#add-coupon-form')[0].reset();
+            });
+        },
+
+        processForm: e => {
+            if (e.preventDefault) {
+                e.preventDefault();
+            }
+
+            this.addCoupon();
+            return false;
+        },
+
+        fillForm: () => {
+            $.get('rest/coupons', function (data) {
+                for (let d of data) {
+                    this.addRow(d);
+                }
+            });
+        },
+
+        addRow: data => {
+            let tr = document.createElement('tr');
+            let id = data.id;
+            let info = [data.product, data.shopName, data.originalPrice, data.discountedPrice, `${Math.round(data.sale)}%`];
+            let cnt = 0;
+
+            for (let i of info) {
+                let td = document.createElement('td');
+                td.appendChild(document.createTextNode(i));
+
+                if (cnt === 2) {
+                    td.classList.add("red-text");
+                } else if (cnt === 3) {
+                    td.classList.add("green-text");
+                }
+
+                tr.appendChild(td);
+                cnt++;
+            }
+
+            restTable.appendChild(tr);
+
+            let btnDelete = document.createElement('button');
+
+            btnDelete.setAttribute('type', 'button');
+            btnDelete.classList.add('btn', 'btn-danger');
+            btnDelete.innerText = 'Delete';
+            btnDelete.addEventListener('click', e => {
+                $.ajax({
+                    type: 'DELETE',
+                    url: 'rest/coupons/' + id,
+                    success: function (item) {
+                        if (item === 'true') {
+                            restTable.removeChild(tr);
+                        }
+                    }
+                });
+            });
+
+            let tdbt = document.createElement('td');
+            tdbt.appendChild(btnDelete);
+            tr.appendChild(tdbt);
+        }
+    };
+
+    $.get('rest/shops', data => {
         for (let d of data) {
             $('#shop-list').append(new Option(d.name, d.id));
         }
     });
 
-    fill();
-
-    let form = document.getElementById('add-coupon-form');
+    result.fillForm();
 
     if (form.attachEvent) {
-        form.attachEvent('submit', processForm);
+        form.attachEvent('submit', result.processForm());
     } else {
-        form.addEventListener('submit', processForm);
-    }
-
-    function createCoupon() {
-        $.post('rest/coupons', $('#add-coupon-form').serialize(), function (data) {
-            addEntry(data);
-            $('#add-coupon-form')[0].reset();
-        });
-    }
-
-    function processForm(e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        }
-
-        createCoupon();
-        return false;
-    }
-
-    function fill() {
-        $.get('rest/coupons', function (data) {
-            for (let d of data) {
-                addEntry(d);
-            }
-        });
-    }
-
-    function addEntry(data) {
-        let tr = document.createElement('tr');
-        let id = data.id;
-        let info = [data.product, data.shopName, data.originalPrice, data.discountedPrice, `${Math.round(data.sale)}%`];
-        let cnt = 0;
-
-        for (let i of info) {
-            let td = document.createElement('td');
-            td.appendChild(document.createTextNode(i));
-
-            if (cnt === 2) {
-                td.classList.add("red-text");
-            } else if (cnt === 3) {
-                td.classList.add("green-text");
-            }
-
-            tr.appendChild(td);
-            cnt++;
-        }
-
-        restTable.appendChild(tr);
-
-        let btnDelete = document.createElement('button');
-
-        btnDelete.setAttribute('type', 'button');
-        btnDelete.classList.add('btn', 'btn-danger');
-        btnDelete.innerText = 'Delete';
-        btnDelete.addEventListener('click', e => {
-            $.ajax({
-                type: 'DELETE',
-                url: 'rest/coupons/' + id,
-                success: function (item) {
-                    if (item === 'true') {
-                        restTable.removeChild(tr);
-                    }
-                }
-            });
-        });
-
-        let tdbt = document.createElement('td');
-        tdbt.appendChild(btnDelete);
-        tr.appendChild(tdbt);
+        form.addEventListener('submit', result.processForm());
     }
 });
